1ae8d09d5ceb0ada774117654f7325d3ffe95f97
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2023-07-06 20:55:10 +0200

    feat: IContentTypeRepository

--- src/Umbraco.Core/Persistence/Repositories/IContentTypeRepository.cs
+++ src/Umbraco.Core/Persistence/Repositories/IContentTypeRepository.cs
@@ -13,10 +13,23 @@ public interface IContentTypeRepository : IContentTypeRepositoryBase<IContentTyp
     IEnumerable<IContentType> GetByQuery(IQuery<PropertyType> query);
 
     /// <summary>
+    ///     Gets all entities of the specified <see cref="IPropertyType" /> query
+    /// </summary>
+    /// <param name="query"></param>
+    /// <returns>An enumerable list of <see cref="IContentType" /> objects</returns>
+    Task<IEnumerable<IContentType>> GetByQueryAsync(IQuery<PropertyType> query, CancellationToken? cancellationToken = null);
+
+    /// <summary>
     ///     Gets all property type aliases.
     /// </summary>
     /// <returns></returns>
     IEnumerable<string> GetAllPropertyTypeAliases();
+
+    /// <summary>
+    ///     Gets all property type aliases.
+    /// </summary>
+    /// <returns></returns>
+    Task<IEnumerable<string>> GetAllPropertyTypeAliasesAsync(CancellationToken? cancellationToken = null);
 
     /// <summary>
     ///     Gets all content type aliases
@@ -28,5 +41,17 @@ public interface IContentTypeRepository : IContentTypeRepositoryBase<IContentTyp
     /// <returns></returns>
     IEnumerable<string> GetAllContentTypeAliases(params Guid[] objectTypes);
 
+    /// <summary>
+    ///     Gets all content type aliases
+    /// </summary>
+    /// <param name="objectTypes">
+    ///     If this list is empty, it will return all content type aliases for media, members and content, otherwise
+    ///     it will only return content type aliases for the object types specified
+    /// </param>
+    /// <returns></returns>
+    Task<IEnumerable<string>> GetAllContentTypeAliasesAsync(CancellationToken? cancellationToken = null, params Guid[] objectTypes);
+
     IEnumerable<int> GetAllContentTypeIds(string[] aliases);
+
+    Task<IEnumerable<int>> GetAllContentTypeIdsAsync(string[] aliases, CancellationToken? cancellationToken = null);
 }
