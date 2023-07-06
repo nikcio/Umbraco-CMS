166e716e87c0e49a91f4ea79c9cc54b770312cd2
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2023-07-06 20:38:13 +0200

    feat: IWriteRepository

--- src/Umbraco.Core/Persistence/IWriteRepository.cs
+++ src/Umbraco.Core/Persistence/IWriteRepository.cs
@@ -11,8 +11,19 @@ public interface IWriteRepository<in TEntity> : IRepository
     void Save(TEntity entity);
 
     /// <summary>
+    ///     Saves an entity.
+    /// </summary>
+    Task SaveAsync(TEntity entity, CancellationToken? cancellation = null);
+
+    /// <summary>
     ///     Deletes an entity.
     /// </summary>
     /// <param name="entity"></param>
     void Delete(TEntity entity);
+
+    /// <summary>
+    ///     Deletes an entity.
+    /// </summary>
+    /// <param name="entity"></param>
+    Task DeleteAsync(TEntity entity, CancellationToken? cancellation = null);
 }
