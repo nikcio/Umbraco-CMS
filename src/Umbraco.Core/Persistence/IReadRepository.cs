bb1ccfc57ba001bf62d9abf18c8b21ce2bbeebae
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2023-07-06 20:37:20 +0200

    feat: IReadRepository

--- src/Umbraco.Core/Persistence/IReadRepository.cs
+++ src/Umbraco.Core/Persistence/IReadRepository.cs
@@ -3,7 +3,7 @@ namespace Umbraco.Cms.Core.Persistence;
 /// <summary>
 ///     Defines the base implementation of a reading repository.
 /// </summary>
-public interface IReadRepository<in TId, out TEntity> : IRepository
+public interface IReadRepository<in TId, TEntity> : IRepository
 {
     /// <summary>
     ///     Gets an entity.
@@ -11,12 +11,27 @@ public interface IReadRepository<in TId, out TEntity> : IRepository
     TEntity? Get(TId? id);
 
     /// <summary>
+    ///     Gets an entity.
+    /// </summary>
+    Task<TEntity?> GetAsync(TId? id, CancellationToken? cancellationToken = null);
+
+    /// <summary>
     ///     Gets entities.
     /// </summary>
     IEnumerable<TEntity> GetMany(params TId[]? ids);
 
     /// <summary>
+    ///     Gets entities.
+    /// </summary>
+    Task<IEnumerable<TEntity>> GetManyAsync(CancellationToken? cancellationToken = null, params TId[]? ids);
+
+    /// <summary>
     ///     Gets a value indicating whether an entity exists.
     /// </summary>
     bool Exists(TId id);
+
+    /// <summary>
+    ///     Gets a value indicating whether an entity exists.
+    /// </summary>
+    Task<bool> ExistsAsync(TId id, CancellationToken? cancellationToken = null);
 }
