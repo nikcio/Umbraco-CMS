5d345cecb2f84723eaad79a7b084d6252fbd0929
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2023-07-06 21:33:11 +0200

    feat: IIdKeyMapRepository

--- src/Umbraco.Core/Persistence/Repositories/IIdKeyMapRepository.cs
+++ src/Umbraco.Core/Persistence/Repositories/IIdKeyMapRepository.cs
@@ -6,5 +6,9 @@ public interface IIdKeyMapRepository
 {
     int? GetIdForKey(Guid key, UmbracoObjectTypes umbracoObjectType);
 
+    Task<int?> GetIdForKeyAsync(Guid key, UmbracoObjectTypes umbracoObjectType, CancellationToken? cancellationToken = null);
+
     Guid? GetIdForKey(int id, UmbracoObjectTypes umbracoObjectType);
+
+    Task<Guid?> GetIdForKeyAsync(int id, UmbracoObjectTypes umbracoObjectType, CancellationToken? cancellationToken = null);
 }
