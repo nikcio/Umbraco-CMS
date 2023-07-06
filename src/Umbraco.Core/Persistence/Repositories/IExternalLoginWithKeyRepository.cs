19963ec923bf39f720689203aee67c94592e056e
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2023-07-06 21:25:53 +0200

    feat: IExternalLoginWithKeyRepository

--- src/Umbraco.Core/Persistence/Repositories/IExternalLoginWithKeyRepository.cs
+++ src/Umbraco.Core/Persistence/Repositories/IExternalLoginWithKeyRepository.cs
@@ -14,12 +14,27 @@ public interface IExternalLoginWithKeyRepository : IReadWriteQueryRepository<int
     void Save(Guid userOrMemberKey, IEnumerable<IExternalLogin> logins);
 
     /// <summary>
+    ///     Replaces all external login providers for the user/member key
+    /// </summary>
+    Task SaveAsync(Guid userOrMemberKey, IEnumerable<IExternalLogin> logins, CancellationToken? cancellationToken = null);
+
+    /// <summary>
     ///     Replaces all external login provider tokens for the providers specified for the user/member key
     /// </summary>
     void Save(Guid userOrMemberKey, IEnumerable<IExternalLoginToken> tokens);
 
     /// <summary>
+    ///     Replaces all external login provider tokens for the providers specified for the user/member key
+    /// </summary>
+    Task SaveAsync(Guid userOrMemberKey, IEnumerable<IExternalLoginToken> tokens, CancellationToken? cancellationToken = null);
+
+    /// <summary>
     ///     Deletes all external logins for the specified the user/member key
     /// </summary>
     void DeleteUserLogins(Guid userOrMemberKey);
+
+    /// <summary>
+    ///     Deletes all external logins for the specified the user/member key
+    /// </summary>
+    Task DeleteUserLoginsAsync(Guid userOrMemberKey, CancellationToken? cancellationToken = null);
 }
