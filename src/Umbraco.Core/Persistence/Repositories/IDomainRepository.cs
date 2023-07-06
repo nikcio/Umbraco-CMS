9791d831945f6ce87e527bb2e73f9ad088577ccc
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2023-07-06 21:16:26 +0200

    feat: IDomainRepository

--- src/Umbraco.Core/Persistence/Repositories/IDomainRepository.cs
+++ src/Umbraco.Core/Persistence/Repositories/IDomainRepository.cs
@@ -6,9 +6,17 @@ public interface IDomainRepository : IReadWriteQueryRepository<int, IDomain>
 {
     IDomain? GetByName(string domainName);
 
+    Task<IDomain?> GetByNameAsync(string domainName, CancellationToken? cancellationToken = null);
+
     bool Exists(string domainName);
+
+    Task<bool> ExistsAsync(string domainName, CancellationToken? cancellationToken = null);
 
     IEnumerable<IDomain> GetAll(bool includeWildcards);
 
+    Task<IEnumerable<IDomain>> GetAllAsync(bool includeWildcards, CancellationToken? cancellationToken = null);
+
     IEnumerable<IDomain> GetAssignedDomains(int contentId, bool includeWildcards);
+
+    Task<IEnumerable<IDomain>> GetAssignedDomainsAsync(int contentId, bool includeWildcards, CancellationToken? cancellationToken = null);
 }
