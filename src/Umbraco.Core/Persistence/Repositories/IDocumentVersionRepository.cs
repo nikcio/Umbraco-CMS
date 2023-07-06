3fadc9375bd7286a99ce500fbc7934a6acad7f58
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2023-07-06 21:14:35 +0200

    feat: IDocumentVersionRepository

--- src/Umbraco.Core/Persistence/Repositories/IDocumentVersionRepository.cs
+++ src/Umbraco.Core/Persistence/Repositories/IDocumentVersionRepository.cs
@@ -10,9 +10,19 @@ public interface IDocumentVersionRepository : IRepository
     public IReadOnlyCollection<ContentVersionMeta>? GetDocumentVersionsEligibleForCleanup();
 
     /// <summary>
+    ///     Gets a list of all historic content versions.
+    /// </summary>
+    public Task<IReadOnlyCollection<ContentVersionMeta>?> GetDocumentVersionsEligibleForCleanupAsync(CancellationToken? cancellationToken = null);
+
+    /// <summary>
     ///     Gets cleanup policy override settings per content type.
     /// </summary>
     public IReadOnlyCollection<ContentVersionCleanupPolicySettings>? GetCleanupPolicies();
+
+    /// <summary>
+    ///     Gets cleanup policy override settings per content type.
+    /// </summary>
+    public Task<IReadOnlyCollection<ContentVersionCleanupPolicySettings>?> GetCleanupPoliciesAsync(CancellationToken? cancellationToken = null);
 
     /// <summary>
     ///     Gets paginated content versions for given content id paginated.
@@ -20,9 +30,19 @@ public interface IDocumentVersionRepository : IRepository
     public IEnumerable<ContentVersionMeta>? GetPagedItemsByContentId(int contentId, long pageIndex, int pageSize, out long totalRecords, int? languageId = null);
 
     /// <summary>
+    ///     Gets paginated content versions for given content id paginated.
+    /// </summary>
+    public Task<(IEnumerable<ContentVersionMeta>? Results, long TotalRecords)> GetPagedItemsByContentIdAsync(int contentId, long pageIndex, int pageSize, int? languageId = null, CancellationToken? cancellationToken = null);
+
+    /// <summary>
     ///     Deletes multiple content versions by ID.
     /// </summary>
     void DeleteVersions(IEnumerable<int> versionIds);
+
+    /// <summary>
+    ///     Deletes multiple content versions by ID.
+    /// </summary>
+    Task DeleteVersionsAsync(IEnumerable<int> versionIds, CancellationToken? cancellationToken = null);
 
     /// <summary>
     ///     Updates the prevent cleanup flag on a content version.
@@ -30,7 +50,17 @@ public interface IDocumentVersionRepository : IRepository
     void SetPreventCleanup(int versionId, bool preventCleanup);
 
     /// <summary>
+    ///     Updates the prevent cleanup flag on a content version.
+    /// </summary>
+    Task SetPreventCleanupAsync(int versionId, bool preventCleanup, CancellationToken? cancellationToken = null);
+
+    /// <summary>
     ///     Gets the content version metadata for a specific version.
     /// </summary>
     ContentVersionMeta? Get(int versionId);
+
+    /// <summary>
+    ///     Gets the content version metadata for a specific version.
+    /// </summary>
+    Task<ContentVersionMeta?> GetAsync(int versionId, CancellationToken? cancellationToken = null);
 }
