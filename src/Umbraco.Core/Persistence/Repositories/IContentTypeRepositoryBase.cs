db8811a078fd7957faaf01a2f51bc13a231d70ca
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2023-07-06 21:00:26 +0200

    feat: IContentTypeRepositoryBase

--- src/Umbraco.Core/Persistence/Repositories/IContentTypeRepositoryBase.cs
+++ src/Umbraco.Core/Persistence/Repositories/IContentTypeRepositoryBase.cs
@@ -11,7 +11,11 @@ public interface IContentTypeRepositoryBase<TItem> : IReadWriteQueryRepository<i
 {
     TItem? Get(string alias);
 
+    Task<TItem?> GetAsync(string alias, CancellationToken? cancellationToken = null);
+
     IEnumerable<MoveEventInfo<TItem>> Move(TItem moving, EntityContainer container);
+
+    Task<IEnumerable<MoveEventInfo<TItem>>> MoveAsync(TItem moving, EntityContainer container, CancellationToken? cancellationToken = null);
 
     /// <summary>
     ///     Derives a unique alias from an existing alias.
@@ -20,6 +24,14 @@ public interface IContentTypeRepositoryBase<TItem> : IReadWriteQueryRepository<i
     /// <returns>The original alias with a number appended to it, so that it is unique.</returns>
     /// <remarks>Unique across all content, media and member types.</remarks>
     string GetUniqueAlias(string alias);
+
+    /// <summary>
+    ///     Derives a unique alias from an existing alias.
+    /// </summary>
+    /// <param name="alias">The original alias.</param>
+    /// <returns>The original alias with a number appended to it, so that it is unique.</returns>
+    /// <remarks>Unique across all content, media and member types.</remarks>
+    Task<string> GetUniqueAliasAsync(string alias, CancellationToken? cancellationToken = null);
 
     /// <summary>
     ///     Gets a value indicating whether there is a list view content item in the path.
@@ -31,12 +43,31 @@ public interface IContentTypeRepositoryBase<TItem> : IReadWriteQueryRepository<i
     /// <summary>
     ///     Gets a value indicating whether there is a list view content item in the path.
     /// </summary>
+    /// <param name="contentPath"></param>
+    /// <returns></returns>
+    Task<bool> HasContainerInPathAsync(string contentPath, CancellationToken? cancellationToken = null);
+
+    /// <summary>
+    ///     Gets a value indicating whether there is a list view content item in the path.
+    /// </summary>
     /// <param name="ids"></param>
     /// <returns></returns>
     bool HasContainerInPath(params int[] ids);
 
     /// <summary>
+    ///     Gets a value indicating whether there is a list view content item in the path.
+    /// </summary>
+    /// <param name="ids"></param>
+    /// <returns></returns>
+    Task<bool> HasContainerInPathAsync(CancellationToken? cancellationToken = null, params int[] ids);
+
+    /// <summary>
     ///     Returns true or false depending on whether content nodes have been created based on the provided content type id.
     /// </summary>
     bool HasContentNodes(int id);
+
+    /// <summary>
+    ///     Returns true or false depending on whether content nodes have been created based on the provided content type id.
+    /// </summary>
+    Task<bool> HasContentNodesAsync(int id, CancellationToken? cancellationToken = null);
 }
