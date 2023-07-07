7ffb1ea9478ae54debc83ce4ab7c47c29be3d679
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2023-07-07 07:24:04 +0200

    feat: ITemplateRepository

--- src/Umbraco.Core/Persistence/Repositories/ITemplateRepository.cs
+++ src/Umbraco.Core/Persistence/Repositories/ITemplateRepository.cs
@@ -6,9 +6,17 @@ public interface ITemplateRepository : IReadWriteQueryRepository<int, ITemplate>
 {
     ITemplate? Get(string? alias);
 
+    Task<ITemplate?> GetAsync(string? alias, CancellationToken? cancellationToken = null);
+
     IEnumerable<ITemplate> GetAll(params string[] aliases);
+
+    Task<IEnumerable<ITemplate>> GetAllAsync(CancellationToken? cancellationToken = null, params string[] aliases);
 
     IEnumerable<ITemplate> GetChildren(int masterTemplateId);
 
+    Task<IEnumerable<ITemplate>> GetChildrenAsync(int masterTemplateId, CancellationToken? cancellationToken = null);
+
     IEnumerable<ITemplate> GetDescendants(int masterTemplateId);
+
+    Task<IEnumerable<ITemplate>> GetDescendantsAsync(int masterTemplateId, CancellationToken? cancellationToken = null);
 }
