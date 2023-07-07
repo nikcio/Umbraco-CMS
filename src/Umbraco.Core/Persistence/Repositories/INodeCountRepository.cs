a5a317d46c0d476ed737ebe793c0de6d690a325a
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2023-07-07 06:56:40 +0200

    feat: INodeCountRepository

--- src/Umbraco.Core/Persistence/Repositories/INodeCountRepository.cs
+++ src/Umbraco.Core/Persistence/Repositories/INodeCountRepository.cs
@@ -4,5 +4,9 @@ public interface INodeCountRepository
 {
     int GetNodeCount(Guid nodeType);
 
+    Task<int> GetNodeCountAsync(Guid nodeType, CancellationToken? cancellationToken = null);
+
     int GetMediaCount();
+
+    Task<int> GetMediaCountAsync(CancellationToken? cancellationToken = null);
 }
