6b64bd1ae46b95ba5daa43787ecf324acf838135
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2023-07-07 07:00:27 +0200

    feat: IPropertyTypeUsageRepository

--- src/Umbraco.Core/Persistence/Repositories/IPropertyTypeUsageRepository.cs
+++ src/Umbraco.Core/Persistence/Repositories/IPropertyTypeUsageRepository.cs
@@ -3,4 +3,6 @@ namespace Umbraco.Cms.Core.Persistence.Repositories;
 public interface IPropertyTypeUsageRepository
 {
     bool HasSavedPropertyValues(string propertyTypeAlias);
+
+    Task<bool> HasSavedPropertyValuesAsync(string propertyTypeAlias, CancellationToken? cancellationToken = null);
 }
