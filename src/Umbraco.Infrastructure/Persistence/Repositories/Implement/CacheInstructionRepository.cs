0e44ce4d20e031679fd6a5215f36322a006d35f8
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2023-07-11 21:25:43 +0200

    feat: CacheInstructionRepository

--- src/Umbraco.Infrastructure/Persistence/Repositories/Implement/CacheInstructionRepository.cs
+++ src/Umbraco.Infrastructure/Persistence/Repositories/Implement/CacheInstructionRepository.cs
@@ -28,23 +28,76 @@ internal class CacheInstructionRepository : ICacheInstructionRepository
         return AmbientScope?.Database.ExecuteScalar<int>(sql) ?? 0;
     }
 
+    public async Task<int> CountAllAsync(CancellationToken? cancellationToken = null)
+    {
+        if (AmbientScope == null)
+        {
+            return 0;
+        }
+
+        Sql<ISqlContext>? sql = AmbientScope.SqlContext.Sql().Select("COUNT(*)")
+            .From<CacheInstructionDto>();
+
+        return await AmbientScope.Database.ExecuteScalarAsync<int>(sql);
+    }
+
     /// <inheritdoc />
     public int CountPendingInstructions(int lastId) =>
         AmbientScope?.Database.ExecuteScalar<int>(
             "SELECT SUM(instructionCount) FROM umbracoCacheInstruction WHERE id > @lastId", new { lastId }) ?? 0;
 
+    public async Task<int> CountPendingInstructionsAsync(int lastId, CancellationToken? cancellationToken = null)
+    {
+        if (AmbientScope == null)
+        {
+            return 0;
+        }
+
+        return await AmbientScope.Database.ExecuteScalarAsync<int>("SELECT SUM(instructionCount) FROM umbracoCacheInstruction WHERE id > @lastId", new { lastId });
+    }
+
     /// <inheritdoc />
     public int GetMaxId() =>
         AmbientScope?.Database.ExecuteScalar<int>("SELECT MAX(id) FROM umbracoCacheInstruction") ?? 0;
 
+    public async Task<int> GetMaxIdAsync(CancellationToken? cancellationToken = null)
+    {
+        if (AmbientScope == null)
+        {
+            return 0;
+        }
+
+        return await AmbientScope.Database.ExecuteScalarAsync<int>("SELECT MAX(id) FROM umbracoCacheInstruction");
+    }
+
     /// <inheritdoc />
     public bool Exists(int id) => AmbientScope?.Database.Exists<CacheInstructionDto>(id) ?? false;
+
+    public Task<bool> ExistsAsync(int id, CancellationToken? cancellationToken = null){
+        if (AmbientScope == null)
+        {
+            return Task.FromResult(false);
+        }
+
+        return Task.FromResult(AmbientScope.Database.Exists<CacheInstructionDto>(id));
+    }
 
     /// <inheritdoc />
     public void Add(CacheInstruction cacheInstruction)
     {
         CacheInstructionDto dto = CacheInstructionFactory.BuildDto(cacheInstruction);
         AmbientScope?.Database.Insert(dto);
+    }
+
+    public async Task AddAsync(CacheInstruction cacheInstruction, CancellationToken? cancellationToken = null)
+    {
+        if(AmbientScope == null)
+        {
+            return;
+        }
+
+        CacheInstructionDto dto = CacheInstructionFactory.BuildDto(cacheInstruction);
+        await AmbientScope.Database.InsertAsync(dto);
     }
 
     /// <inheritdoc />
@@ -59,6 +112,21 @@ internal class CacheInstructionRepository : ICacheInstructionRepository
                Array.Empty<CacheInstruction>();
     }
 
+    public async Task<IEnumerable<CacheInstruction>> GetPendingInstructionsAsync(int lastId, int maxNumberToRetrieve, CancellationToken? cancellationToken = null)
+    {
+        if (AmbientScope == null)
+        {
+            return Enumerable.Empty<CacheInstruction>();
+        }
+
+        Sql<ISqlContext>? sql = AmbientScope.SqlContext.Sql().SelectAll()
+            .From<CacheInstructionDto>()
+            .Where<CacheInstructionDto>(dto => dto.Id > lastId)
+            .OrderBy<CacheInstructionDto>(dto => dto.Id);
+        Sql<ISqlContext>? topSql = sql?.SelectTop(maxNumberToRetrieve);
+        return (await AmbientScope.Database.FetchAsync<CacheInstructionDto>(topSql)).Select(CacheInstructionFactory.BuildEntity);
+    }
+
     /// <inheritdoc />
     public void DeleteInstructionsOlderThan(DateTime pruneDate)
     {
@@ -69,5 +137,21 @@ internal class CacheInstructionRepository : ICacheInstructionRepository
                 @"DELETE FROM umbracoCacheInstruction WHERE utcStamp < @pruneDate AND id < @maxId",
                 new { pruneDate, maxId });
         AmbientScope?.Database.Execute(deleteSql);
+    }
+
+    public async Task DeleteInstructionsOlderThanAsync(DateTime pruneDate, CancellationToken? cancellationToken = null)
+    {
+        if(AmbientScope == null)
+        {
+            return;
+        }
+
+        // Using 2 queries is faster than convoluted joins.
+        var maxId = await AmbientScope.Database.ExecuteScalarAsync<int>("SELECT MAX(id) FROM umbracoCacheInstruction;");
+        Sql deleteSql =
+            new Sql().Append(
+                @"DELETE FROM umbracoCacheInstruction WHERE utcStamp < @pruneDate AND id < @maxId",
+                new { pruneDate, maxId });
+        await AmbientScope.Database.ExecuteAsync(deleteSql);
     }
 }
