b2dc5cebe538533010a46842b064389e3f07ca0e
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2023-07-13 16:38:11 +0200

    feat: PermissionRepository (Partial)

--- src/Umbraco.Infrastructure/Persistence/Repositories/Implement/PermissionRepository.cs
+++ src/Umbraco.Infrastructure/Persistence/Repositories/Implement/PermissionRepository.cs
@@ -90,6 +90,65 @@ internal class PermissionRepository<TEntity> : EntityRepositoryBase<int, Content
     }
 
     /// <summary>
+    ///     Returns explicitly defined permissions for a user group for any number of nodes
+    /// </summary>
+    /// <param name="groupIds">
+    ///     The group ids to lookup permissions for
+    /// </param>
+    /// <param name="entityIds"></param>
+    /// <returns></returns>
+    /// <remarks>
+    ///     This method will not support passing in more than 2000 group IDs when also passing in entity IDs.
+    /// </remarks>
+    public async Task<EntityPermissionCollection> GetPermissionsForEntitiesAsync(int[] groupIds, CancellationToken? cancellationToken = null, params int[] entityIds)
+    {
+        var result = new EntityPermissionCollection();
+
+        if (entityIds.Length == 0)
+        {
+            foreach (IEnumerable<int> group in groupIds.InGroupsOf(Constants.Sql.MaxParameterCount))
+            {
+                Sql<ISqlContext> sql = Sql()
+                    .SelectAll()
+                    .From<UserGroup2NodeDto>()
+                    .LeftJoin<UserGroup2NodePermissionDto>().On<UserGroup2NodeDto, UserGroup2NodePermissionDto>(
+                        (left, right) => left.NodeId == right.NodeId && left.UserGroupId == right.UserGroupId)
+                    .Where<UserGroup2NodeDto>(dto => group.Contains(dto.UserGroupId));
+
+                List<UserGroup2NodePermissionDto> permissions =
+                    await AmbientScope.Database.FetchAsync<UserGroup2NodePermissionDto>(sql);
+                foreach (EntityPermission permission in ConvertToPermissionList(permissions))
+                {
+                    result.Add(permission);
+                }
+            }
+        }
+        else
+        {
+            foreach (IEnumerable<int> group in entityIds.InGroupsOf(Constants.Sql.MaxParameterCount -
+                                                                    groupIds.Length))
+            {
+                Sql<ISqlContext> sql = Sql()
+                    .SelectAll()
+                    .From<UserGroup2NodeDto>()
+                    .LeftJoin<UserGroup2NodePermissionDto>().On<UserGroup2NodeDto, UserGroup2NodePermissionDto>(
+                        (left, right) => left.NodeId == right.NodeId && left.UserGroupId == right.UserGroupId)
+                    .Where<UserGroup2NodeDto>(dto =>
+                        groupIds.Contains(dto.UserGroupId) && group.Contains(dto.NodeId));
+
+                List<UserGroup2NodePermissionDto> permissions =
+                    await AmbientScope.Database.FetchAsync<UserGroup2NodePermissionDto>(sql);
+                foreach (EntityPermission permission in ConvertToPermissionList(permissions))
+                {
+                    result.Add(permission);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    /// <summary>
     ///     Returns permissions directly assigned to the content items for all user groups
     /// </summary>
     /// <param name="entityIds"></param>
@@ -110,6 +169,26 @@ internal class PermissionRepository<TEntity> : EntityRepositoryBase<int, Content
     }
 
     /// <summary>
+    ///     Returns permissions directly assigned to the content items for all user groups
+    /// </summary>
+    /// <param name="entityIds"></param>
+    /// <returns></returns>
+    public async Task<IEnumerable<EntityPermission>> GetPermissionsForEntitiesAsync(int[] entityIds, CancellationToken? cancellationToken = null)
+    {
+        Sql<ISqlContext> sql = Sql()
+            .SelectAll()
+            .From<UserGroup2NodeDto>()
+            .LeftJoin<UserGroup2NodePermissionDto>()
+            .On<UserGroup2NodeDto, UserGroup2NodePermissionDto>((left, right) =>
+                left.NodeId == right.NodeId && left.UserGroupId == right.UserGroupId)
+            .Where<UserGroup2NodeDto>(dto => entityIds.Contains(dto.NodeId))
+            .OrderBy<UserGroup2NodeDto>(dto => dto.NodeId);
+
+        List<UserGroup2NodePermissionDto> result = await AmbientScope.Database.FetchAsync<UserGroup2NodePermissionDto>(sql);
+        return ConvertToPermissionList(result);
+    }
+
+    /// <summary>
     ///     Returns permissions directly assigned to the content item for all user groups
     /// </summary>
     /// <param name="entityId"></param>
@@ -126,6 +205,26 @@ internal class PermissionRepository<TEntity> : EntityRepositoryBase<int, Content
             .OrderBy<UserGroup2NodeDto>(dto => dto.NodeId);
 
         List<UserGroup2NodePermissionDto> result = AmbientScope.Database.Fetch<UserGroup2NodePermissionDto>(sql);
+        return ConvertToPermissionList(result);
+    }
+
+    /// <summary>
+    ///     Returns permissions directly assigned to the content item for all user groups
+    /// </summary>
+    /// <param name="entityId"></param>
+    /// <returns></returns>
+    public async Task<EntityPermissionCollection> GetPermissionsForEntityAsync(int entityId, CancellationToken? cancellationToken = null)
+    {
+        Sql<ISqlContext> sql = Sql()
+            .SelectAll()
+            .From<UserGroup2NodeDto>()
+            .LeftJoin<UserGroup2NodePermissionDto>()
+            .On<UserGroup2NodeDto, UserGroup2NodePermissionDto>((left, right) =>
+                left.NodeId == right.NodeId && left.UserGroupId == right.UserGroupId)
+            .Where<UserGroup2NodeDto>(dto => dto.NodeId == entityId)
+            .OrderBy<UserGroup2NodeDto>(dto => dto.NodeId);
+
+        List<UserGroup2NodePermissionDto> result = await AmbientScope.Database.FetchAsync<UserGroup2NodePermissionDto>(sql);
         return ConvertToPermissionList(result);
     }
 
@@ -177,6 +276,55 @@ internal class PermissionRepository<TEntity> : EntityRepositoryBase<int, Content
     }
 
     /// <summary>
+    ///     Assigns the same permission set for a single group to any number of entities
+    /// </summary>
+    /// <param name="groupId"></param>
+    /// <param name="permissions">The permissions to assign or null to remove the connection between group and entityIds</param>
+    /// <param name="entityIds"></param>
+    /// <remarks>
+    ///     This will first clear the permissions for this user and entities and recreate them
+    /// </remarks>
+    public async Task ReplacePermissionsAsync(int groupId, IEnumerable<char>? permissions, CancellationToken? cancellationToken = null, params int[] entityIds)
+    {
+        if (entityIds.Length == 0)
+        {
+            return;
+        }
+
+        IUmbracoDatabase db = AmbientScope.Database;
+
+        foreach (IEnumerable<int> group in entityIds.InGroupsOf(Constants.Sql.MaxParameterCount))
+        {
+            await db.ExecuteAsync("DELETE FROM umbracoUserGroup2Node WHERE userGroupId = @groupId AND nodeId in (@nodeIds)", new { groupId, nodeIds = group });
+
+            await db.ExecuteAsync("DELETE FROM umbracoUserGroup2NodePermission WHERE userGroupId = @groupId AND nodeId in (@nodeIds)", new { groupId, nodeIds = group });
+        }
+
+        if (permissions is not null)
+        {
+            var toInsert = new List<UserGroup2NodeDto>();
+            var toInsertPermissions = new List<UserGroup2NodePermissionDto>();
+
+            foreach (var e in entityIds)
+            {
+                toInsert.Add(new UserGroup2NodeDto { NodeId = e, UserGroupId = groupId });
+                foreach (var p in permissions)
+                {
+                    toInsertPermissions.Add(new UserGroup2NodePermissionDto
+                    {
+                        NodeId = e,
+                        Permission = p.ToString(CultureInfo.InvariantCulture),
+                        UserGroupId = groupId,
+                    });
+                }
+            }
+
+            await db.BulkInsertRecordsAsync(toInsert, cancellationToken);
+            await db.BulkInsertRecordsAsync(toInsertPermissions, cancellationToken);
+        }
+    }
+
+    /// <summary>
     ///     Assigns one permission for a user to many entities
     /// </summary>
     /// <param name="groupId"></param>
@@ -201,6 +349,35 @@ internal class PermissionRepository<TEntity> : EntityRepositoryBase<int, Content
 
         db.BulkInsertRecords(actions);
         db.BulkInsertRecords(actionsPermissions);
+    }
+
+    /// <summary>
+    ///     Assigns one permission for a user to many entities
+    /// </summary>
+    /// <param name="groupId"></param>
+    /// <param name="permission"></param>
+    /// <param name="entityIds"></param>
+    public async Task AssignPermissionAsync(int groupId, char permission, CancellationToken? cancellationToken = null, params int[] entityIds)
+    {
+        IUmbracoDatabase db = AmbientScope.Database;
+
+        await db.ExecuteAsync("DELETE FROM umbracoUserGroup2Node WHERE userGroupId = @groupId AND nodeId in (@entityIds)", new { groupId, entityIds });
+        await db.ExecuteAsync(
+            "DELETE FROM umbracoUserGroup2NodePermission WHERE userGroupId = @groupId AND permission=@permission AND nodeId in (@entityIds)",
+            new { groupId, permission = permission.ToString(CultureInfo.InvariantCulture), entityIds });
+
+        UserGroup2NodeDto[] actionsPermissions =
+            entityIds.Select(id => new UserGroup2NodeDto { NodeId = id, UserGroupId = groupId }).ToArray();
+
+        UserGroup2NodePermissionDto[] actions = entityIds.Select(id => new UserGroup2NodePermissionDto
+        {
+            NodeId = id,
+            Permission = permission.ToString(CultureInfo.InvariantCulture),
+            UserGroupId = groupId,
+        }).ToArray();
+
+        await db.BulkInsertRecordsAsync(actions, cancellationToken);
+        await db.BulkInsertRecordsAsync(actionsPermissions, cancellationToken);
     }
 
     /// <summary>
@@ -239,6 +416,44 @@ internal class PermissionRepository<TEntity> : EntityRepositoryBase<int, Content
     }
 
     /// <summary>
+    ///     Assigns one permission to an entity for multiple groups
+    /// </summary>
+    /// <param name="entity"></param>
+    /// <param name="permission"></param>
+    /// <param name="groupIds"></param>
+    public async Task AssignEntityPermissionAsync(TEntity entity, char permission, IEnumerable<int> groupIds, CancellationToken? cancellationToken = null)
+    {
+        IUmbracoDatabase db = AmbientScope.Database;
+        var groupIdsA = groupIds.ToArray();
+
+        await db.ExecuteAsync("DELETE FROM umbracoUserGroup2Node WHERE nodeId = @nodeId AND userGroupId in (@groupIds)", new { nodeId = entity.Id, groupIds = groupIdsA });
+        await db.ExecuteAsync(
+            "DELETE FROM umbracoUserGroup2NodePermission WHERE nodeId = @nodeId AND permission = @permission AND userGroupId in (@groupIds)",
+            new
+            {
+                nodeId = entity.Id,
+                permission = permission.ToString(CultureInfo.InvariantCulture),
+                groupIds = groupIdsA,
+            });
+
+        UserGroup2NodePermissionDto[] actionsPermissions = groupIdsA.Select(id => new UserGroup2NodePermissionDto
+        {
+            NodeId = entity.Id,
+            Permission = permission.ToString(CultureInfo.InvariantCulture),
+            UserGroupId = id,
+        }).ToArray();
+
+        UserGroup2NodeDto[] actions = groupIdsA.Select(id => new UserGroup2NodeDto
+        {
+            NodeId = entity.Id,
+            UserGroupId = id,
+        }).ToArray();
+
+        await db.BulkInsertRecordsAsync(actions);
+        await db.BulkInsertRecordsAsync(actionsPermissions);
+    }
+
+    /// <summary>
     ///     Assigns permissions to an entity for multiple group/permission entries
     /// </summary>
     /// <param name="permissionSet">
@@ -274,6 +489,45 @@ internal class PermissionRepository<TEntity> : EntityRepositoryBase<int, Content
 
         db.BulkInsertRecords(toInsert);
         db.BulkInsertRecords(toInsertPermissions);
+    }
+
+    /// <summary>
+    ///     Assigns permissions to an entity for multiple group/permission entries
+    /// </summary>
+    /// <param name="permissionSet">
+    /// </param>
+    /// <remarks>
+    ///     This will first clear the permissions for this entity then re-create them
+    /// </remarks>
+    public async Task ReplaceEntityPermissionsAsync(EntityPermissionSet permissionSet, CancellationToken? cancellationToken = null)
+    {
+        IUmbracoDatabase db = AmbientScope.Database;
+
+        await db.ExecuteAsync("DELETE FROM umbracoUserGroup2Node WHERE nodeId = @nodeId", new { nodeId = permissionSet.EntityId });
+        await db.ExecuteAsync("DELETE FROM umbracoUserGroup2NodePermission WHERE nodeId = @nodeId", new { nodeId = permissionSet.EntityId });
+
+        var toInsert = new List<UserGroup2NodeDto>();
+        var toInsertPermissions = new List<UserGroup2NodePermissionDto>();
+        foreach (EntityPermission entityPermission in permissionSet.PermissionsSet)
+        {
+            toInsert.Add(new UserGroup2NodeDto
+            {
+                NodeId = permissionSet.EntityId,
+                UserGroupId = entityPermission.UserGroupId,
+            });
+            foreach (var permission in entityPermission.AssignedPermissions)
+            {
+                toInsertPermissions.Add(new UserGroup2NodePermissionDto
+                {
+                    NodeId = permissionSet.EntityId,
+                    Permission = permission,
+                    UserGroupId = entityPermission.UserGroupId,
+                });
+            }
+        }
+
+        await db.BulkInsertRecordsAsync(toInsert, cancellationToken);
+        await db.BulkInsertRecordsAsync(toInsertPermissions, cancellationToken);
     }
 
     /// <summary>
