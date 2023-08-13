30c037cfd71ed3701ff8ccc75afc5423b314bd2f
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2023-08-13 11:51:08 +0200

    feat: EFCoreDatabaseRepositoryBase

---
+++ src/Umbraco.Cms.Persistence.EFCore/Repositories/EFCoreDatabaseRepositoryBase.cs
@@ -0,0 +1,26 @@
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Models.Entities;
+using Umbraco.Cms.Infrastructure.Persistence.Repositories;
+using Umbraco.Cms.Persistence.EFCore.DbContexts;
+
+namespace Umbraco.Cms.Persistence.EFCore.Repositories;
+
+/// <summary>
+/// Represents the base class for all EFCore database repositories.
+/// </summary>
+internal abstract class EFCoreDatabaseRepositoryBase : DatabaseRepositoryBase
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="EFCoreDatabaseRepositoryBase"/> class.
+    /// </summary>
+    /// <param name="dbContext"></param>
+    protected EFCoreDatabaseRepositoryBase(UmbracoDbContext dbContext)
+    {
+        Context = dbContext;
+    }
+
+    /// <summary>
+    /// Gets the database context.
+    /// </summary>
+    protected UmbracoDbContext Context { get; }
+}
