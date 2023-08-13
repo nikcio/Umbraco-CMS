ced849dfca2a2d4eaa5ec6f6d6d55c00a875f3c7
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2023-08-13 11:50:40 +0200

    feat: DatabaseRepositoryBase

---
+++ src/Umbraco.Infrastructure/Persistence/Repositories/DatabaseRepositoryBase.cs
@@ -0,0 +1,12 @@
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Models.Entities;
+using Umbraco.Cms.Core.Persistence.Repositories;
+
+namespace Umbraco.Cms.Infrastructure.Persistence.Repositories;
+
+/// <summary>
+/// Represents the base class for all database repositories.
+/// </summary>
+public abstract class DatabaseRepositoryBase : IDatabaseRepository
+{
+}
