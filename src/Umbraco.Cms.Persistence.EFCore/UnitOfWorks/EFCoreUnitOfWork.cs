03f68e17c048a231f52ae5c59acd5a7dd716de26
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2023-08-13 11:50:09 +0200

    feat: EFCoreUnitOfWork

---
+++ src/Umbraco.Cms.Persistence.EFCore/UnitOfWorks/EFCoreUnitOfWork.cs
@@ -0,0 +1,74 @@
+using System.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Umbraco.Cms.Core.DistributedLocking;
+using Umbraco.Cms.Core.Persistence.Repositories;
+using Umbraco.Cms.Infrastructure.Persistence.UnitOfWorks;
+using Umbraco.Cms.Persistence.EFCore.DbContexts;
+
+namespace Umbraco.Cms.Persistence.EFCore.UnitOfWorks;
+
+/// <summary>
+/// Represents a unit of work for a specific <see cref="IDatabaseRepository"/>.
+/// </summary>
+internal class EFCoreUnitOfWork : UnitOfWorkBase
+{
+    private readonly UmbracoDbContext _dbContext;
+    private readonly ILogger<EFCoreUnitOfWork> _logger;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="EFCoreUnitOfWork"/> class.
+    /// </summary>
+    /// <param name="dbContext"></param>
+    /// <param name="loggerFactory"></param>
+    /// <param name="lockingMechanismFactory"></param>
+    public EFCoreUnitOfWork(
+        ILoggerFactory loggerFactory,
+        IDistributedLockingMechanismFactory lockingMechanismFactory,
+        UmbracoDbContext dbContext)
+        : base(loggerFactory, lockingMechanismFactory)
+    {
+        _dbContext = dbContext;
+        _logger = loggerFactory.CreateLogger<EFCoreUnitOfWork>();
+    }
+
+    /// <inheritdoc/>
+    public override void Complete()
+    {
+        try
+        {
+            var modifiedEntries = _dbContext.SaveChanges();
+            _logger.LogDebug("Saved {ModifiedEntries} entries to the database", modifiedEntries);
+        }
+        catch (DbUpdateConcurrencyException updateConcurrencyException)
+        {
+            _logger.LogError(updateConcurrencyException, "Encountered concurrency error while saving changes to the database");
+            throw;
+        }
+        catch (DbUpdateException updateException)
+        {
+            _logger.LogError(updateException, "Encountered update exception while saving changes to the database");
+            throw;
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception, "Error saving changes to the database");
+            throw;
+        }
+        finally
+        {
+            LockingMechanism.ClearLocks(InstanceId);
+        }
+    }
+
+    /// <inheritdoc/>
+    public override void Start(IsolationLevel? isolationLevel = null)
+    {
+        if (isolationLevel.HasValue)
+        {
+            _dbContext.Database.BeginTransaction(isolationLevel.Value);
+        }
+
+        _dbContext.Database.BeginTransaction();
+    }
+}
